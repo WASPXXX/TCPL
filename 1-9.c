38b57d8edfce9f011e5a3ea5d2bbb84fbca6012e
lls <ryannotdead@gmail.com>
2019-03-15 10:04:53 +0800

    in->out multi spaces->one space

---
+++ 1-9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define NONBLANK 'a'
+
+int main() {
+	int c,lastc;
+	lastc = NONBLANK;
+
+	while ((c = getchar()) != EOF) {
+		if (c != ' ') {
+			putchar(c);
+		}
+		else if (lastc != ' ') {
+			putchar(c);
+		}
+		lastc = c;
+	}
+/*	while((c = getchar()) != EOf) {
+		if(c != ' ' || lastc != ' ') {
+			putchar(c);
+		}
+		lastc = c;
+	}*/
+	return 0;
+}
