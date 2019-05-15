61cd0da8c4eb0a71cc5e01e269fff474ef35ce40
lls <ryannotdead@gmail.com>
2019-05-15 16:25:53 +0800

    switch2

--- 3-2.c
+++ 3-2.c
@@ -5,78 +5,78 @@ void escape_new(char s[], char t[]); /*字符串t复制到字符串s中，并将
 void unescape_new(char s[], char t[]); /*相反*/
 
 int main() {
-	char line[MAXLINE], cline[MAXLINE];
-	while (getline_new(line, MAXLINE) > 0) {
-		printf("input:\t %s\n", line);
-		unescape_new(cline, line);
-		printf("output:\t %s\n", cline);
+    char line[MAXLINE], cline[MAXLINE];
+    while (getline_new(line, MAXLINE) > 0) {
+        printf("input:\t %s\n", line);
+        unescape_new(cline, line);
+        printf("output:\t %s\n", cline);
         printf("dd\n");
-	}
+    }
 }
 
 void escape_new(char s[], char t[]) {
-	int i, j;
-	i = j = 0;
+    int i, j;
+    i = j = 0;
 
-	while (t[i] != '\0') {
-		switch(t[i]) {
-			case '\n':
-				s[j++] = '\\';
-				s[j++] = 'n';
-				i++;
-				break;
-			case '\t':
-				s[j++] = '\\';
-				s[j++] = 't';
-				i++;
-				break;
-			default:
-				s[j++] = t[i++];
-				break;
-		}
-	}
-	s[j] = '\0';
+    while (t[i] != '\0') {
+        switch(t[i]) {
+            case '\n':
+                s[j++] = '\\';
+                s[j++] = 'n';
+                i++;
+                break;
+            case '\t':
+                s[j++] = '\\';
+                s[j++] = 't';
+                i++;
+                break;
+            default:
+                s[j++] = t[i++];
+                break;
+        }
+    }
+    s[j] = '\0';
 }
 
 void unescape_new(char s[], char t[]) {
-	int i, j;
-	i = j = 0;
+    int i, j;
+    i = j = 0;
 
-	while (t[i] != '\0') {
-		switch(t[i]) {
-			case '\\':
-				switch(t[++i]) { 		//注意这里i++和++i使用的区别
-					case 'n':
-						s[j++] = '\n';
-						break;
-					case 't':
-						s[j++] = '\t';
-						break;
-					default:
-						s[j++] = '\\';
-						s[j++] = t[i];
-						break;
-				}
-				i++;
-				break;
-			default:
-				s[j++] = t[i++];
-				break;
-		}
-	}
-	s[j] = '\0';
+    while (t[i] != '\0') {
+        switch(t[i]) {
+            case '\\':
+                switch(t[++i]) { 		//注意这里i++和++i使用的区别
+                    case 'n':
+                        s[j++] = '\n';
+                        break;
+                    case 't':
+                        s[j++] = '\t';
+                        break;
+                    default:
+                        s[j++] = '\\';
+                        s[j++] = t[i];
+                        break;
+                }
+                i++;
+                break;
+            default:
+                s[j++] = t[i++];
+                break;
+        }
+    }
+    s[j] = '\0';
 }
 
 int getline_new(char s[], int lim) {
-	int c, i;
+    int c, i;
 
-	for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i) {
-		s[i] = c;
-	}
-	if (c == '\n') {
-		s[i] = c;
-		++i;
-	}
-	s[i] = '\0';
-	return i; //i即是含换行符的字符串长度
+    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i; //i即是含换行符的字符串长度
 }
